1a7be3d8015db735d0fad11f0dfce8cff1ede420
Nicolas Duma <nicolasduma@outlook.com>
2022-05-18 14:11:53 -0300

    ✨ feat(web): add BrowserRouter component in App

--- packages/web/src/App/index.tsx
+++ packages/web/src/App/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 
 const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <BrowserRouter>
+      <h1>Hello World</h1>
+    </BrowserRouter>
+  )
 }
 
 export default App
