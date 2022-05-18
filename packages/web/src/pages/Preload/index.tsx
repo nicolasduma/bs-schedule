66ac6569bcf8cb283877117cac238e315a70e263
Nicolas Duma <nicolasduma@outlook.com>
2022-05-18 14:14:16 -0300

    ✨ feat(web): create PreloadPage

---
+++ packages/web/src/pages/Preload/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PreloadPage = () => {
+  return <h1>PreloadPage</h1>
+}
+
+export default PreloadPage
---
+++ packages/web/src/pages/index.ts
@@ -0,0 +1,1 @@
+export { default as PreloadPage } from './Preload'
