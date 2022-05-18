0f37b170cf4d3883d9e4a3eae35afca9b9535066
Nicolas Duma <nicolasduma@outlook.com>
2022-05-18 20:08:14 -0300

    🔧 chore(web): add twin.d.ts in types folder

---
+++ packages/web/types/twin.d.ts
@@ -0,0 +1,27 @@
+import 'twin.macro'
+import styledImport, { CSSProp, css as cssImport } from 'styled-components'
+
+declare module 'twin.macro' {
+  const styled: typeof styledImport
+  const css: typeof cssImport
+}
+
+declare module 'react' {
+  interface HTMLAttributes<T> extends DOMAttributes<T> {
+    css?: CSSProp
+    tw?: string
+  }
+  /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+  interface SVGProps<T> extends SVGProps<SVGSVGElement> {
+    css?: CSSProp
+    tw?: string
+  }
+}
+
+declare global {
+  namespace JSX {
+    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
+      as?: string | Element
+    }
+  }
+}
