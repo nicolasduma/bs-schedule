e7715bb0f81b22881be5ac03a49d934067039eb1
Nicolas Duma <nicolasduma@outlook.com>
2022-05-18 18:14:37 -0300

    🔧 chore(web): set darkslategray color to tailwind

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkslategray: {
+          700: '#2b3138',
+          800: '#262A31',
+          900: '#202329',
+        },
+      },
+    },
   },
   plugins: [],
 }
