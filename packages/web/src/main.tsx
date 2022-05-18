c6f36ce94d431d344c2aa1171dde20ea02aaf934
Nicolas Duma <nicolasduma@outlook.com>
2022-05-18 20:14:37 -0300

    ✨ feat(web): replace main.css with GlobalStyle in main.tsx

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from '#/App'
-import '#/main.css'
+import GlobalStyle from '#/styles/GlobalStyle'
 
-/* eslint-disable  @typescript-eslint/no-non-null-assertion */
+/* eslint-disable-next-line  @typescript-eslint/no-non-null-assertion */
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 )
