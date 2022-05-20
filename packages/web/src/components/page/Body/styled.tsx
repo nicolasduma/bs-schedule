9c247c2609d2dc95fccc7db8f9782a42de8afb45
Nicolas Duma <nicolasduma@outlook.com>
2022-05-19 22:48:18 -0300

    ✨ feat(web): add style to PreloadPage

---
+++ packages/web/src/components/page/Body/styled.tsx
@@ -0,0 +1,6 @@
+import tw from 'twin.macro'
+
+export const Body = tw.div`
+  min-h-screen
+  flex items-center justify-center
+`
--- packages/web/src/pages/Preload/index.tsx
+++ packages/web/src/pages/Preload/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 import { PageComponent } from '#/components'
+
+import * as Styled from './styled'
+
 const PreloadPage = () => {
+  return (
     <PageComponent.Body>
+      <Styled.Container>
+      </Styled.Container>
     </PageComponent.Body>
+  )
 }
 
 export default PreloadPage
