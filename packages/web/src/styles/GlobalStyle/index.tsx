e2e5d9ec20fa8ab8e824f90063540a6b0efacc44
Nicolas Duma <nicolasduma@outlook.com>
2022-05-19 22:29:38 -0300

    ✨ feat(web): remove colors classes of GlobalStyle

--- packages/web/src/styles/GlobalStyle/index.tsx
+++ packages/web/src/styles/GlobalStyle/index.tsx
@@ -6,7 +6,7 @@ import tw, { theme, GlobalStyles as BaseStyles } from 'twin.macro'
 const CustomStyles = createGlobalStyle({
   body: {
     WebkitTapHighlightColor: theme``,
-    ...tw`antialiased bg-darkslategray-900 text-white text-opacity-95`,
+    ...tw`antialiased`,
   },
 })
 
