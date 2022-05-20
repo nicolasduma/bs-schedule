d135abe75435639a2f541a13d25eaa3ecd859840
Nicolas Duma <nicolasduma@outlook.com>
2022-05-19 22:36:42 -0300

    🔧 chore(web): add amber color with tone 400 in tailwindcss

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        amber: {
+          400: '#fbbf24',
+        },
         darkslategray: {
           700: '#2b3138',
           800: '#262A31',
