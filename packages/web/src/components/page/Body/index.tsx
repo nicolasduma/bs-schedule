055ee80a9f0a8562719e271ec520158b6623fa2d
Nicolas Duma <nicolasduma@outlook.com>
2022-05-19 22:45:14 -0300

    ✨ feat(web): add style to Body page component

--- packages/web/src/components/page/Body/index.tsx
+++ packages/web/src/components/page/Body/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import * as Styled from './styled'
+
 interface PropsInterface {
   children?: any
 }
 
-const Body = ({ children }: PropsInterface) => <div>{children}</div>
+const Body = ({ children }: PropsInterface) => (
+  <Styled.Body>{children}</Styled.Body>
+)
 
 export default Body
