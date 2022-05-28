b0f798fd1a280082fef44623157fef00fd90a19d
Nicolas Duma <nicolasduma@outlook.com>
2022-05-28 15:54:17 -0300

    ✨ feat(web): add AccessPage to Routes

--- packages/web/src/App/Routes/index.tsx
+++ packages/web/src/App/Routes/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Routes as RoutesContainer, Route } from 'react-router-dom'
 
-import { PreloadPage } from '#/pages'
+import { PreloadPage, AccessPage } from '#/pages'
 
 const Routes = () => (
   <RoutesContainer>
     <Route path="/" element={<PreloadPage />} />
+    <Route path="/acessar" element={<AccessPage />} />
   </RoutesContainer>
 )
 
