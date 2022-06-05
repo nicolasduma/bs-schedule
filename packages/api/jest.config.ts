db6bf99ae2a7cdc903432e9953e7de71d195818f
Nicolas Duma <nicolasduma@outlook.com>
2022-06-05 14:01:52 -0300

    🔧 chore(api): update jest.config.ts to support ts-paths

--- packages/api/jest.config.ts
+++ packages/api/jest.config.ts
@@ -1,7 +1,15 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+import { compilerOptions } from './tsconfig.json'
+
 export default {
   clearMocks: true,
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>',
+  }),
+  preset: 'ts-jest',
 }
