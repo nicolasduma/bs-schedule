4c1a61ce1ca4fa2548b983a438bf21fed6e13709
Nicolas Duma <nicolasduma@outlook.com>
2022-06-07 17:19:28 -0300

    ✨ feat(api): add ERROS to constants

--- packages/api/src/constants/index.ts
+++ packages/api/src/constants/index.ts
@@ -1,1 +1,6 @@
 export const PORT = process.env.PORT || 7777
+
+export const ERRORS = {
+  invalidPassword: { code: 11, msg: 'invalid-password' },
+  invalidEmail: { code: 12, msg: 'invalid-email' },
+}
