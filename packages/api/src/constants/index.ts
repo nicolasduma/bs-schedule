348d9db943e19290a68d3a6b6400aedb8fe77930
Nicolas Duma <nicolasduma@outlook.com>
2022-06-16 15:26:00 -0300

    :recycle: refactor(api): remove constant ERRORS

--- packages/api/src/constants/index.ts
+++ packages/api/src/constants/index.ts
@@ -1,6 +1,1 @@
 export const PORT = process.env.PORT || 7777
-
-export const ERRORS = {
-  invalidPassword: { code: 11, msg: 'invalid-password' },
-  invalidEmail: { code: 12, msg: 'invalid-email' },
-}
