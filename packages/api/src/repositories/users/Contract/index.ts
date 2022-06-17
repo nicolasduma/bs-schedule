3c1d6e108bf7d113374c11757a5b07a9be42fb47
Nicolas Duma <nicolasduma@outlook.com>
2022-06-17 20:13:35 -0300

    ✨ feat(api): add findByEmail to usersRepository

--- packages/api/src/repositories/users/Contract/index.ts
+++ packages/api/src/repositories/users/Contract/index.ts
@@ -1,7 +1,9 @@
 import { userEntity } from '#/entities'
+import { EmailType } from '#/entities/User/Contract/types'
 
 interface Contract {
   save: (datas: userEntity.Contract) => Promise<void>
+  findByEmail: (email: EmailType) => Promise<userEntity.Contract | null>
 }
 
 export default Contract
--- packages/api/src/repositories/users/adapter/index.ts
+++ packages/api/src/repositories/users/adapter/index.ts
@@ -1,6 +1,7 @@
 import prisma from '#/libs/prisma'
 
 import { userEntity } from '#/entities'
+import { EmailType } from '#/entities/User/Contract/types'
 
 import { Contract } from '../'
 
@@ -13,6 +14,13 @@ const adapter: Contract = {
     }
     await prisma.user.create({ data: filtedDatas })
   },
+
+  findByEmail: async (email: EmailType) =>
+    await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    }),
 }
 
 export default adapter
