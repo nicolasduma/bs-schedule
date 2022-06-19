a487c372f57a1b464198f530334eed408dd180a4
Nicolas Duma <nicolasduma@outlook.com>
2022-06-19 17:05:43 -0300

    ✨ feat(api): add layer to erros in validateUser of userEntity

--- packages/api/src/entities/User/validateUser/index.ts
+++ packages/api/src/entities/User/validateUser/index.ts
@@ -3,21 +3,25 @@ import { makeResult } from '#/utils'
 import { Contract, validateEmail, validatePassword } from '..'
 
 const validateUser = (user: Contract) => {
-  if (!user.email) return makeResult({ message: 'email/not-found' })
+  const layer = 'entity'
+  if (!user.email) return makeResult({ layer, message: 'email/not-found' })
 
   const { error: emailValidationError } = validateEmail(user.email)
 
   if (emailValidationError)
     return makeResult({
+      layer,
       message: emailValidationError.message,
     })
 
-  if (!user.password) return makeResult({ message: 'password/not-found' })
+  if (!user.password)
+    return makeResult({ layer, message: 'password/not-found' })
 
   const { error: passwordValidationError } = validatePassword(user.password)
 
   if (passwordValidationError)
     return makeResult({
+      layer,
       message: passwordValidationError.message,
     })
 
