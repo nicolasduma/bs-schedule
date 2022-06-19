0ee6a2118914cf285bb566cf141227486696fe3d
Nicolas Duma <nicolasduma@outlook.com>
2022-06-19 17:56:55 -0300

    :recycle: refactor(api): update props of usersRepository Contract
    Add readonly to props

--- packages/api/src/repositories/users/Contract/index.ts
+++ packages/api/src/repositories/users/Contract/index.ts
@@ -2,8 +2,10 @@ import { userEntity } from '#/entities'
 import { EmailType } from '#/entities/User/Contract/types'
 
 interface Contract {
-  save: (datas: userEntity.Contract) => Promise<void>
-  findByEmail: (email: EmailType) => Promise<userEntity.Contract | null>
+  readonly save: (datas: userEntity.Contract) => Promise<void>
+  readonly findByEmail: (
+    email: EmailType
+  ) => Promise<userEntity.Contract | null>
 }
 
 export default Contract
