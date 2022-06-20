522ee06843d817ba17131448742e01176c640a7c
Nicolas Duma <nicolasduma@outlook.com>
2022-06-20 15:36:55 -0300

    ✨ feat(api): add verifyUserFeacture to router

--- packages/api/src/app/router/user/index.ts
+++ packages/api/src/app/router/user/index.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express'
 
-import { registerUserFeacture } from '#/feactures'
+import { registerUserFeacture, verifyUser } from '#/feactures'
 
 const userRouter = Router()
 
 userRouter.post('/register', (req, res, next) =>
   registerUserFeacture.handleController(req, res, next)
+)
+
+userRouter.post('/verify', (req, res, next) =>
+  verifyUser.handleController(req, res, next)
 )
 
 export default userRouter
