a87ffbebed5933c29b27a1d46eab268b192006ae
Nicolas Duma <nicolasduma@outlook.com>
2022-06-21 14:50:39 -0300

    ✨ feat(api): add cors to app

--- packages/api/src/app/index.ts
+++ packages/api/src/app/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 
 import bodyParcer from 'body-parser'
+import cors from 'cors'
 
 import router from './router'
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParcer.json())
 app.use(router)
 
