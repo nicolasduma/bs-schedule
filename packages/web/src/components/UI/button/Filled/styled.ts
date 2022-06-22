9fa4a7ea445317060862043f56de9e4f1981264c
Nicolas Duma <nicolasduma@outlook.com>
2022-06-22 20:52:53 -0300

    :recycle: refactor(web): update styled of UIComponent.Button.Filled

--- packages/web/src/components/UI/button/Filled/styled.ts
+++ packages/web/src/components/UI/button/Filled/styled.ts
@@ -8,8 +8,9 @@ export const Filled = tw.button`
   text-darkslategray-900
   font-medium
   outline-none
+  transition-colors
 
-  hover:bg-amber-300 transition-colors
+  hover:bg-amber-300 
   focus:(ring-2 ring-amber-400 ring-offset-2 ring-offset-darkslategray-900)
   disabled:(opacity-60 cursor-default hover:bg-amber-400)
 `
