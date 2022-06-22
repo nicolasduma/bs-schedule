f1e77c5f66ee9f150250151a6a2112870b1a7228
Nicolas Duma <nicolasduma@outlook.com>
2022-06-22 20:51:08 -0300

    ✨ feat(web): add animations to PageComponent.Body

--- packages/web/src/components/page/Body/index.tsx
+++ packages/web/src/components/page/Body/index.tsx
@@ -1,13 +1,44 @@
 import React from 'react'
 
+import { HTMLMotionProps } from 'framer-motion'
+
 import * as Styled from './styled'
 
-interface PropsInterface {
-  children?: any
+interface PropsInterface extends HTMLMotionProps<'div'> {
+  animateToShow?: boolean
+  animateToHidden?: boolean
 }
 
-const Body = ({ children }: PropsInterface) => (
-  <Styled.Body>{children}</Styled.Body>
+const Body = ({
+  animateToShow = true,
+  animateToHidden = true,
+  ...props
+}: PropsInterface) => (
+  <Styled.Body
+    initial={animateToShow ? { opacity: 0 } : {}}
+    animate={
+      animateToShow
+        ? {
+            opacity: 1,
+            transition: {
+              duration: 1,
+              delay: 1,
+            },
+          }
+        : {}
+    }
+    exit={
+      animateToHidden
+        ? {
+            opacity: 0,
+            transition: {
+              duration: 1,
+            },
+          }
+        : {}
+    }
+    {...props}
+  />
 )
 
 export default Body
--- packages/web/src/components/page/Body/styled.tsx
+++ packages/web/src/components/page/Body/styled.tsx
@@ -1,6 +1,9 @@
+import { motion } from 'framer-motion'
 import tw from 'twin.macro'
 
-export const Body = tw.div`
-  min-h-screen
+export const Body = tw(motion.div)`
+  w-full
+  h-full
   flex items-center justify-center
+  absolute
 `
