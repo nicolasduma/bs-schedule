b91a2c481ac6fdda766831999907eb1054c35664
Nicolas Duma <nicolasduma@outlook.com>
2022-07-01 23:20:55 -0300

    ✨ feat(web): add store to App

--- packages/web/src/App/index.tsx
+++ packages/web/src/App/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { HelmetProvider } from 'react-helmet-async'
+import { Provider as ReduxProvider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+
+import store from '#/store'
 
 import AnimatedRoutes from './AnimatedRoutes'
 import * as Styled from './styled'
@@ -8,11 +11,13 @@ import * as Styled from './styled'
 const App = () => {
   return (
     <Styled.App>
-      <HelmetProvider>
-        <BrowserRouter>
-          <AnimatedRoutes />
-        </BrowserRouter>
-      </HelmetProvider>
+      <ReduxProvider store={store}>
+        <HelmetProvider>
+          <BrowserRouter>
+            <AnimatedRoutes />
+          </BrowserRouter>
+        </HelmetProvider>
+      </ReduxProvider>
     </Styled.App>
   )
 }
