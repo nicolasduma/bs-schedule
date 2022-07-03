002580b51959673e26a4b01447bd06a6d6d4ca00
Nicolas Duma <nicolasduma@outlook.com>
2022-07-03 13:58:08 -0300

    ✨ feat(web): update styled of Form of AccessPage

--- packages/web/src/pages/Access/Form/styled.ts
+++ packages/web/src/pages/Access/Form/styled.ts
@@ -1,20 +1,25 @@
-import tw from 'twin.macro'
+import tw, { styled } from 'twin.macro'
+
+import { UIComponent } from '#/components'
 
 export const Form = tw.form`
   w-full
   flex flex-col justify-center items-center gap-4
 `
 
-export const RevealPasswordButton = tw.button`
+export const RevealPasswordButton = styled(UIComponent.Button.Core)`
+  ${tw`
+
   mr-4
   p-1
-  relative
+  border-[2px] border-transparent
   rounded-md
+  relative
   text-2xl text-zinc-600
-  border-[2px]
-  border-transparent
   outline-none
   focus:border-amber-400
 
   [:before]:(content-[''] w-full h-full absolute left-0 top-0)
+  
+  `}
 `
