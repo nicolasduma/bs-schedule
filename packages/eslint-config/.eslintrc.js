340640c65162edb699d4f97353f00b620577ab31
Nicolas Duma <nicolasduma@outlook.com>
2022-07-02 23:32:27 -0300

    🔧 chore(eslint-config): ignore more rules

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -46,5 +46,8 @@ module.exports = {
       },
     ],
     '@typescript-eslint/no-explicit-any': ['off'],
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react/no-children-prop': 'off',
   },
 }
