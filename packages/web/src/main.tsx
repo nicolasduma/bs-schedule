3524f29c0fc472b32daef5a822839bdae6e9aea6
Nicolas Duma <nicolasduma@outlook.com>
2022-07-04 13:50:02 -0300

    :recycle: refactor(web): remove comment in main.ts

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom/client'
 import App from '#/App'
 import GlobalStyle from '#/styles/GlobalStyle'
 
-/* eslint-disable-next-line  @typescript-eslint/no-non-null-assertion */
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyle />
