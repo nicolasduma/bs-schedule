b89cfca71735e2f14eda5cedfb6bbeae6c239b7c
Nicolas Duma <nicolasduma@outlook.com>
2022-07-05 18:39:57 -0300

    :recycle: refactor(web): update styled of AccessPage

--- packages/web/src/pages/Access/styled.ts
+++ packages/web/src/pages/Access/styled.ts
@@ -1,12 +1,10 @@
 import tw from 'twin.macro'
 
-import { UIComponent } from '#/components'
-
 export const Container = tw.div`
   w-full max-w-xl
   flex flex-col items-center gap-6
   px-4
-`
+  `
 
 export const LogoContainer = tw.div`
   w-60
@@ -20,16 +18,4 @@ export const OrLine = tw.div`
   tracking-widest
 
   [:before, :after]:(content-[''] w-full h-0.5 bg-darkslategray-700)
-`
-
-export const AccessWithGoogleButton = tw(UIComponent.Button.Filled)`
-  px-[5%]
-  flex justify-center items-center
-  bg-darkslategray-800
-  text-zinc-100
-  cursor-pointer
-  [:hover]:bg-darkslategray-700
-
-  [> :nth-child(1)]:(text-xl)
-  [> span]:flex-grow
 `
