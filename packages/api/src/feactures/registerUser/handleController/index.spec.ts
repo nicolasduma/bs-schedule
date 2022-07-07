c4a4c7bf7b7d59809f286f370e85973fe1024454
Nicolas Duma <nicolasduma@outlook.com>
2022-07-07 15:28:22 -0300

    :test_tube: test(api): update tests of registerUserFeacture controller

--- packages/api/src/feactures/registerUser/handleController/index.spec.ts
+++ packages/api/src/feactures/registerUser/handleController/index.spec.ts
@@ -6,20 +6,28 @@ import useRequest from 'supertest'
 
 import app from '#/app'
 
+import { userEntity } from '#/entities'
+
+const userTest: userEntity.Contract = {
+  authMethod: 'form',
+  email: 'contact@email.com',
+  password: '123456',
+}
+
 describe('handle controller of register user', () => {
   it('should be able register user', async () => {
-    const resposne = await useRequest(app).post('/user/register').send({
-      email: 'contact@email.com',
-      password: '123456',
-    })
+    const resposne = await useRequest(app).post('/user/register').send(userTest)
 
     expect(resposne.status).toBe(201)
   })
 
   it('should not be able register user without email', async () => {
-    const resposne = await useRequest(app).post('/user/register').send({
-      password: '123456',
-    })
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { email, ...userTestWithoutEmail } = userTest
+
+    const resposne = await useRequest(app)
+      .post('/user/register')
+      .send(userTestWithoutEmail)
 
     expect(resposne.status).toBe(400)
     expect(resposne.body.error.message).toBe('email/not-found')
@@ -27,10 +35,7 @@ describe('handle controller of register user', () => {
   })
 
   it('should not be able register user with email that already exists', async () => {
-    const resposne = await useRequest(app).post('/user/register').send({
-      email: 'contact@email.com',
-      password: '123456',
-    })
+    const resposne = await useRequest(app).post('/user/register').send(userTest)
 
     expect(resposne.status).toBe(400)
     expect(resposne.body.error.message).toBe('email/already-exists')
