7f98db67e1a9fbccc575f9048b062be12f77c7c1
Nicolas Duma <nicolasduma@outlook.com>
2022-07-07 13:48:08 -0300

    ✨ feat(api): add validateAuthMethod to userEntity.validateUser

--- packages/api/src/entities/User/validateUser/index.spec.ts
+++ packages/api/src/entities/User/validateUser/index.spec.ts
@@ -2,6 +2,7 @@ import validateUser from '.'
 import { Contract } from '../'
 
 const testUser: Contract = {
+  authMethod: 'form',
   email: 'contact@email.com',
   password: '123456',
 }
@@ -10,6 +11,24 @@ describe('validate user to make it', () => {
   it('should be able validate', () => {
     const { success } = validateUser(testUser)
     expect(success).toBe(true)
+  })
+
+  it('should not be able to validate without authMethod', () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { authMethod, ...testUserCopy } = testUser
+    // @ts-ignore
+    const { error } = validateUser(testUserCopy)
+
+    expect(error.message).toBe('authMethod/not-found')
+    expect(error.layer).toBe('entity')
+  })
+
+  it('should not be able to validate with invalid authMethod', () => {
+    // @ts-ignore
+    const { error } = validateUser({ ...testUser, authMethod: ' ' })
+
+    expect(error.message).toBeTruthy()
+    expect(error.layer).toBe('entity')
   })
 
   it('should not be able to validate without email', () => {
--- packages/api/src/entities/User/validateUser/index.ts
+++ packages/api/src/entities/User/validateUser/index.ts
@@ -1,9 +1,28 @@
 import { makeResult } from '@bs-schedule/utils'
 
-import { Contract, validateEmail, validatePassword } from '..'
+import {
+  Contract,
+  validateAuthMethod,
+  validateEmail,
+  validatePassword,
+} from '..'
 
 const validateUser = (user: Contract) => {
   const layer = 'entity'
+
+  if (!user.authMethod)
+    return makeResult({ layer, message: 'authMethod/not-found' })
+
+  const { error: authMethodValidationError } = validateAuthMethod(
+    user.authMethod
+  )
+
+  if (authMethodValidationError)
+    return makeResult({
+      layer,
+      message: authMethodValidationError.message,
+    })
+
   if (!user.email) return makeResult({ layer, message: 'email/not-found' })
 
   const { error: emailValidationError } = validateEmail(user.email)
