4c2353f2f3d7e5caf9484788527600be7c809238
Nicolas Duma <nicolasduma@outlook.com>
2022-07-07 15:27:26 -0300

    :test_tube: test(api): update tests of registerUserFeacture interactor

--- packages/api/src/feactures/registerUser/makeExecuteInteractor/index.spec.ts
+++ packages/api/src/feactures/registerUser/makeExecuteInteractor/index.spec.ts
@@ -1,10 +1,13 @@
+import { userEntity } from '#/entities'
+
 import { usersRepository } from '#/repositories'
 
 import makeExecuteInteractor from '.'
 
 jest.spyOn(usersRepository.inMemory, 'save')
 
-const testUser = {
+const testUser: userEntity.Contract = {
+  authMethod: 'form',
   email: 'contato@email.com',
   password: '123456',
 }
@@ -13,14 +16,16 @@ const executeInteractor = makeExecuteInteractor(usersRepository.inMemory)
 
 describe('make execute interactor of register user', () => {
   it('should be able register user', async () => {
-    const { success } = await executeInteractor(testUser)
+    const { success } = await executeInteractor({ user: testUser })
 
     expect(success).toBe(true)
     expect(usersRepository.inMemory.save).toHaveBeenCalled()
   })
 
   it('should not be able register user with invalid email', async () => {
-    const { error } = await executeInteractor({ ...testUser, email: '' })
+    const { error } = await executeInteractor({
+      user: { ...testUser, email: '' },
+    })
 
     expect(error.message).toBeTruthy()
   })
