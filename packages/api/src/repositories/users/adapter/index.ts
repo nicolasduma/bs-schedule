2d30ccf25a63eed18d06db1eaa1a8eba50b4c376
Nicolas Duma <nicolasduma@outlook.com>
2022-07-07 14:51:41 -0300

    :recycle: refactor(api): update usersRepository.adapter

--- packages/api/src/repositories/users/adapter/index.ts
+++ packages/api/src/repositories/users/adapter/index.ts
@@ -1,26 +1,32 @@
 import prisma from '#/libs/prisma'
 
 import { userEntity } from '#/entities'
-import { EmailType } from '#/entities/User/Contract/types'
+import { EmailType } from '#/entities/User/Contract'
 
 import { Contract } from '../'
 
 const adapter: Contract = {
   save: async (user: userEntity.Contract) => {
     const filtedDatas: userEntity.Contract = {
+      id: user.id,
+      authMethod: user.authMethod,
       email: user.email,
       password: user.password,
-      id: user.id,
     }
+
     await prisma.user.create({ data: filtedDatas })
   },
 
-  findByEmail: async (email: EmailType) =>
-    await prisma.user.findUnique({
+  findByEmail: async (email: EmailType) => {
+    // @ts-ignore
+    const user: userEntity.Contract = await prisma.user.findUnique({
       where: {
         email,
       },
-    }),
+    })
+
+    return user
+  },
 }
 
 export default adapter
