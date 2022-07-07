9f1d290d3683385645b79cdf3d463ffe21d5f5d1
Nicolas Duma <nicolasduma@outlook.com>
2022-07-07 14:53:24 -0300

    :recycle: refactor(api): update usersRepository.inMemory

--- packages/api/src/repositories/users/inMemory/index.ts
+++ packages/api/src/repositories/users/inMemory/index.ts
@@ -1,26 +1,34 @@
 import { generatePasswordHash } from '#/utils'
 
 import { userEntity } from '#/entities'
-import { EmailType } from '#/entities/User/Contract/types'
+import { EmailType } from '#/entities/User/Contract'
 
 import { Contract } from '../'
 
 const { success: user1 } = userEntity.make({
+  authMethod: 'form',
   email: 'contact1@email.com',
   password: '123456',
 })
 const { success: user2 } = userEntity.make({
   email: 'contact2@email.com',
+  authMethod: 'form',
   password: '123456',
 })
-const usersArray = [
-  { ...user1, password: generatePasswordHash(user1.password) },
-  { ...user2, password: generatePasswordHash(user2.password) },
+const usersArray: userEntity.Contract[] = [
+  {
+    ...user1,
+    password: generatePasswordHash(user1.password),
+  },
+  {
+    ...user2,
+    password: generatePasswordHash(user2.password),
+  },
 ]
 
 const inMemory: Contract = {
   save: async () => {
-    console.log()
+    await usersArray.push(user1)
   },
 
   findByEmail: async (email: EmailType) => {
