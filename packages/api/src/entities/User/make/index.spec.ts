e07a7a62743b48166cd86c7a895203886ee6a2fa
Nicolas Duma <nicolasduma@outlook.com>
2022-07-07 13:51:44 -0300

    :test_tube: test(api): update tests of userEntity.make

--- packages/api/src/entities/User/make/index.spec.ts
+++ packages/api/src/entities/User/make/index.spec.ts
@@ -5,13 +5,15 @@ import make from '.'
 import { Contract } from '..'
 
 const testUser: Contract = {
-  email: 'contato@email.com',
+  authMethod: 'form',
+  email: 'contact@email.com',
   password: '123456',
 }
 
 describe('make user', () => {
   it('should be able make a user', () => {
     const { success: user } = make(testUser)
+
     expect(user.email).toBe(testUser.email)
     expect(user.password).toBe(testUser.password)
     expect(validateUUID(user.id)).toBe(true)
@@ -19,6 +21,7 @@ describe('make user', () => {
 
   it('should not be able make user without email', () => {
     const { error } = make({ ...testUser, email: '' })
+
     expect(error).not.toBeFalsy()
     expect(error.layer).toBe('entity')
   })
