dd065294fa6220f2a5f58402ad124cfe61894efc
Nicolas Duma <nicolasduma@outlook.com>
2022-07-08 22:11:02 -0300

    :recycle: refactor(api): add types to UserEntity

---
+++ packages/api/src/entities/User/types/index.ts
@@ -0,0 +1,5 @@
+export type IdType = string
+export type AuthMethodType = 'form' | 'google'
+export type EmailType = string
+export type PasswordType = string
+export type AccessTokenType = string | null
