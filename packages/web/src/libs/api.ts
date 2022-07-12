94608f79b427c7ac820cae1d6c5be1d3fb56f8a7
Nicolas Duma <nicolasduma@outlook.com>
2022-07-11 22:36:49 -0300

    ✨ feat(web): update api in libs

--- packages/web/src/libs/api.ts
+++ packages/web/src/libs/api.ts
@@ -2,6 +2,11 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
 })
 
 export default api
