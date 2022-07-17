8230724f79ac56ce59d525b307b74c7e33282455
Nicolas Duma <nicolasduma@outlook.com>
2022-07-17 13:57:00 -0300

    ✨ feat(api): add corsOptions to app

--- packages/api/src/app/index.ts
+++ packages/api/src/app/index.ts
@@ -7,7 +7,14 @@ import router from './router'
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+  origin: true,
+  preflightContinue: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}
+
+app.use(cors(corsOptions))
 app.use(bodyParcer.json())
 app.use(router)
 
