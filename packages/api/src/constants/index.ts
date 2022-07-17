2cfbfe4280b77c6310dc49cbc001e5edc11bd43e
Nicolas Duma <nicolasduma@outlook.com>
2022-07-17 13:59:07 -0300

    ✨ feat(api): add ACCESS_TOKEN_COOKIE to constants

--- packages/api/src/constants/index.ts
+++ packages/api/src/constants/index.ts
@@ -1,1 +1,10 @@
+import { CookieOptions } from 'express'
+
 export const PORT = process.env.PORT || 7777
+
+export const ACCESS_TOKEN_COOKIE = {
+  name: 'accessToken',
+  options: {
+    httpOnly: true,
+  } as CookieOptions,
+}
