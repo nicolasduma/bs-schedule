5dcc6aa2fe4d1b7613b5498e3ab45df0b7c77f3f
Nicolas Duma <nicolasduma@outlook.com>
2022-07-18 09:15:05 -0300

    ✨ feat(web): add userScreen state to UserPage Screens

--- packages/web/src/pages/User/Screens/index.tsx
+++ packages/web/src/pages/User/Screens/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import { useStateSelector } from '#/hooks'
 import HomeScreen from './Home'
 import * as Styled from './styled'
 
 const Screens = () => {
+  const { userScreen } = useStateSelector((state) => state)
+
   return (
     <Styled.ScreenContant>
       <HomeScreen />
