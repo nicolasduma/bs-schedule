aca64f204f27e47e61aec7270d65efb6d86eb9d6
Nicolas Duma <nicolasduma@outlook.com>
2022-07-20 22:25:37 -0300

    :recycle: refactor(web): update styled of PageComponent.Body

---
+++ packages/web/src/components/page/Body/styled.ts
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion'
+import tw, { styled } from 'twin.macro'
+
+export const Body = styled(motion.div)`
+  ${tw`
+  
+  w-full
+  min-h-screen
+  flex justify-center
+  absolute
+  
+  `}
+`
--- packages/web/src/components/page/Body/styled.tsx
+++
@@ -1,9 +0,0 @@
-import { motion } from 'framer-motion'
-import tw from 'twin.macro'
-
-export const Body = tw(motion.div)`
-  w-full
-  h-full
-  flex items-center justify-center
-  absolute
-`
