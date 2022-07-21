68742df80f232e900c059ec7eb900ebd582878bf
Nicolas Duma <nicolasduma@outlook.com>
2022-07-20 22:23:38 -0300

    ✨ feat(web): update GlobalStyle in styles

--- packages/web/src/styles/GlobalStyle/index.tsx
+++ packages/web/src/styles/GlobalStyle/index.tsx
@@ -8,24 +8,23 @@ const CustomStyles = createGlobalStyle({
     WebkitTapHighlightColor: theme``,
     ...tw`
       
-    min-h-[600px] h-screen
     bg-darkslategray-900
     text-white text-opacity-95
     antialiased
     -webkit-tap-highlight-color[transparent]
 
     [::-webkit-scrollbar]:(hidden)
-    [> div#root]:(h-full)
+    [> div#root]:(w-full)
     
     `,
   },
 })
 
-const GlobalStyles = () => (
+const GlobalStyle = () => (
   <>
     <BaseStyles />
     <CustomStyles />
   </>
 )
 
-export default GlobalStyles
+export default GlobalStyle
