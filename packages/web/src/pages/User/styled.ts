6e3078f6ea669b77ea3c92471fadd4e557dbda32
Nicolas Duma <nicolasduma@outlook.com>
2022-07-20 22:30:42 -0300

    :recycle: refactor(web): update styled of UserPage

--- packages/web/src/pages/User/styled.ts
+++ packages/web/src/pages/User/styled.ts
@@ -2,12 +2,13 @@ import tw from 'twin.macro'
 
 export const Container = tw.div`
   w-full
-  h-full
+  min-h-full
   flex flex-col
 `
 
 export const ScreensContainer = tw.div`
   w-full
-  py-6
+  flex-grow
+  pt-6
   flex justify-center
 `
