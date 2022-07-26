a6b0367f6fd985d91a9a1851234d6d654c3128fa
Nicolas Duma <nicolasduma@outlook.com>
2022-07-25 22:35:41 -0300

    ✨ feat(web): add months to constants

---
+++ packages/web/src/constants/index.ts
@@ -0,0 +1,14 @@
+export const months = [
+  { initials: 'jan', name: 'janeiro' },
+  { initials: 'fev', name: 'fevereiro' },
+  { initials: 'mar', name: 'março' },
+  { initials: 'abr', name: 'abril' },
+  { initials: 'maio', name: 'maio' },
+  { initials: 'jun', name: 'junho' },
+  { initials: 'jul', name: 'julho' },
+  { initials: 'ago', name: 'agosto' },
+  { initials: 'set', name: 'setembro' },
+  { initials: 'out', name: 'outubro' },
+  { initials: 'nov', name: 'novembro' },
+  { initials: 'dez', name: 'dezembro' },
+]
