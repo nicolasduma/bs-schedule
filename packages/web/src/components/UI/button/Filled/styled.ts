a735c9f7233a036b6e178274b3658341a174c5ff
Nicolas Duma <nicolasduma@outlook.com>
2022-07-26 16:02:17 -0300

    ✨ feat(web): update styled of UIComponent.Button.Filled

--- packages/web/src/components/UI/button/Filled/styled.ts
+++ packages/web/src/components/UI/button/Filled/styled.ts
@@ -4,7 +4,7 @@ import { Core } from '../'
 
 export const Filled = tw(Core)`
   w-full
-  h-14
+  h-12
   bg-amber-400
   rounded-md
   text-darkslategray-900
@@ -12,7 +12,8 @@ export const Filled = tw(Core)`
   outline-none
   transition-colors
 
-  hover:bg-amber-300 
+  hover:bg-amber-300
   focus:(ring-2 ring-amber-400 ring-offset-2 ring-offset-darkslategray-900)
+  active:(ring-0 ring-offset-0)
   disabled:(opacity-60 cursor-default hover:bg-amber-400)
 `
