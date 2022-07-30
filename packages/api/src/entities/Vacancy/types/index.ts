5802352a2e7d4fbca9583c08bb816b2ed982a922
Nicolas Duma <nicolasduma@outlook.com>
2022-07-29 22:33:48 -0300

    ✨ feat(api): add types to Vacancy in entities

---
+++ packages/api/src/entities/Vacancy/types/index.ts
@@ -0,0 +1,10 @@
+export type IdType = string
+export type BarberIdType = string
+export type UserIdType = string
+export type TimestampType = number
+
+export interface ServicesContract {
+  gradient: boolean
+  beard: boolean
+  eyebrow: boolean
+}
